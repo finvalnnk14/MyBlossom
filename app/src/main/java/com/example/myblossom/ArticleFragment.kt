d48746e1bb57b3f2bc570e94993bf63c07481e27
Fina Valentina <finavalenn1402@gmail.com>
2024-05-06 16:16:43 +0700

    bug fix list Article

--- app/src/main/java/com/example/myblossom/ArticleFragment.kt
+++ app/src/main/java/com/example/myblossom/ArticleFragment.kt
@@ -21,35 +21,35 @@ class ArticleFragment : Fragment(){
 
 
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+   override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
-        super.onCreate(savedInstanceState)
-        //setContentView(R.layout.activity_main)
-        val root = inflater.inflate(R.layout.fragment_article, container, false)
+       super.onCreate(savedInstanceState)
+       //setContentView(R.layout.activity_main)
+       val root = inflater.inflate(R.layout.fragment_article, container, false)
 
-        // getting the recyclerview by its id
-        val recyclerview = root.findViewById<RecyclerView>(R.id.recyclerview)
+       // getting the recyclerview by its id
+       val recyclerview = root.findViewById<RecyclerView>(R.id.recyclerview)
 
-        // this creates a vertical layout Manager
-        recyclerview.layoutManager = LinearLayoutManager(activity)
+       // this creates a vertical layout Manager
+       recyclerview.layoutManager = LinearLayoutManager(activity)
 
-        // ArrayList of class ItemsViewModel
-        val data = ArrayList<ItemsViewModel>()
+       // ArrayList of class ItemsViewModel
+       val data = ArrayList<ItemsViewModel>()
 
-        // This loop will create 20 Views containing
-        // the image with the count of view
-        //for (i in 1..20) {
-        //data.add(ItemsViewModel(R.drawable.mood2, "Item " + i))
-        // }
+       // This loop will create 20 Views containing
+       // the image with the count of view
+       //for (i in 1..20) {
+           //data.add(ItemsViewModel(R.drawable.mood2, "Item " + i))
+      // }
 
-        data.add(ItemsViewModel(R.drawable.arti1, ""))
-        data.add(ItemsViewModel(R.drawable.arti2, ""))
+       data.add(ItemsViewModel(R.drawable.arti1, ""))
+       data.add(ItemsViewModel(R.drawable.arti2, ""))
 
-        // This will pass the ArrayList to our Adapter
-        val adapter = CustomAdapter(data)
+       // This will pass the ArrayList to our Adapter
+       val adapter = CustomAdapter(data)
 
-        // Setting the Adapter with the recyclerview
-        recyclerview.adapter = adapter
+       // Setting the Adapter with the recyclerview
+       recyclerview.adapter = adapter
 //        val root = inflater.inflate(R.layout.fragment_article, container, false)
 
 //        val arrayAdapter: ArrayAdapter<*>
@@ -69,18 +69,18 @@ class ArticleFragment : Fragment(){
 //        var mListView = root.findViewById<ListView>(R.id.articleList)
 
 //       val imageView : ImageView = mListView.findViewById(R.id.modd)
-        // var textView : TextView = mListView.findViewById(R.id.moddtxt)
+       // var textView : TextView = mListView.findViewById(R.id.moddtxt)
         //var textView1 : TextView = view.findViewById(R.id.descTv)
         //arrayAdapter = ArrayAdapter(root.context, android.R.layout.simple_list_item_1, users)
-        // arrayAdapter = ArrayAdapter(root.context, R.layout.article_list, users)
-        // arrayAdapter = ArrayAdapter(root.context, R.layout.article_list, users)
+       // arrayAdapter = ArrayAdapter(root.context, R.layout.article_list, users)
+       // arrayAdapter = ArrayAdapter(root.context, R.layout.article_list, users)
 //        mListView.adapter = arrayAdapter
 
         //articleList
-        // val languages = listOf("Java", "Kotlin", "Javascript", "PHP", "Python")
+       // val languages = listOf("Java", "Kotlin", "Javascript", "PHP", "Python")
         //return inflater.inflate(R.layout.fragment_article,container,false)
         return root
-    }
+   }
 
 
 
